7297f86b5a95ac7f075823e8fa9ba1551274f3c1
Williams Argenal <williamsargenal@gmail.com>
2019-03-18 16:54:59 -0700

    got some whiteboarding

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 'use strict';
 
+/*
+build something that will 
 
-//build something that will 
-// - select 3 random phots
-// - display them side by side 
-// - make pic's clickable
-// -track those clicks 
-// -track how man times img is displayed on side to not repeat so much 
-// - 
+ - select 3 random phots
+ - an array of 25 images(start with three img)
+XXX array of clicks [0, 0, 0]
 
-//global var
+ array of object, 
+ -[{Pic}, {Pic}]
+ - pic object:
+  {image url: "picture name.jpg,
+   clicks: 0,
+name: name of pic,
+}
+  - listen for an event ('click')
+ - randomly selected three photos from libary
+ - display them side by side 
+ - make pic's clickable
+ -track those clicks 
+ -track how man times img is displayed on side to not repeat so much 
+ - 
+
+*/
