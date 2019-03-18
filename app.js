89ac69b7f93cc7fd6013d7b665dd570bb7ca2b22
Williams Argenal <williamsargenal@gmail.com>
2019-03-18 16:21:56 -0700

    updating pictures?

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+//build something that will 
+// - select 3 random phots
+// - display them side by side 
+// - make pic's clickable
+// -track those clicks 
+// -track how man times img is displayed on side to not repeat so much 
+// - 
+
+//global var
