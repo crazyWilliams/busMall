3ca1ba075e30a5a03077369feddd9735589e4781
Williams Argenal <williamsargenal@gmail.com>
2019-03-18 21:21:01 -0700

    made some elements

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ build something that will
 
 tracks voted pictures clicked counts
 
- - select 3 random phots
+ - select 3 random photos -
  - an array of 25 images(start with three img)
-XXX array of clicks [0, 0, 0]
+
 
  array of object, 
  -[{Pic}, {Pic}]
@@ -50,6 +50,31 @@ var imageInfo = function(url, name){
 //==============================================================//
 //   Initialize the Page
 //==============================================================//
+ var leftImage = document.getElementById('left_image');
+ var rightImage = document.getElementById('right_image');
+ var centerImage = document.getElementById('center_image');
+
+function handleClickOnLeftImage(event){
+  // increment left image clicks
+  console.log('clicked on left image')
+}
+
+function handleClickOnRightImage(event){
+  // increment right image clicks
+  console.log('clicked on right image')
+}
+
+function handleClickOnCenterImage(event){
+  // increment center image clicks
+  console.log('clicked on center image')
+}
+
+ leftImage.addEventListener('click', handleClickOnLeftImage);
+
+ rightImage.addEventListener('click', handleClickOnRighImage);
+
+ centerImage.addEventListener('click', handleClickOnCenterImage);
+ 
 
 new imageInfo('bag.jpg', 'starwars bag');
 new imageInfo('banana.jpg', 'banana');
