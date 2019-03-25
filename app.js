366c9f6514a418de823b6bd5c52c955d92808d89
Williams Argenal <williamsargenal@gmail.com>
2019-03-24 18:23:15 -0700

    trying to upload something

--- app.js
+++ app.js
@@ -90,10 +90,6 @@ function handleClickOnImage (clickEvent){
   }
 
   totalClicks++;
-  //figure out which image was clicked
-  // This broke if i didnt click on an image
-  // Do this for all images
-
   for( var i = 0; i < allImages.length; i++){
     if(event.target.src.includes(allImages[i].filePath)){
       console.log(`${allImages[i].name} was picked`);
@@ -101,12 +97,13 @@ function handleClickOnImage (clickEvent){
     }
   }
   pickThreeImagesAndIncrementAppeared();
-  // check if i have clicked 5 times, then put the chart on
-  if(totalClicks > 25){
-    buildMyChart();
+  if(totalClicks > 3){
+   buildMyChart();
   }
 
 }
+
+//=================================================================//
 
 // Chartjs
 function buildMyChart(){
@@ -115,9 +112,9 @@ function buildMyChart(){
 
   var labels = [];
   var data = [];
-  for(var i = 0; i < allImages.length; i++){
-    labels.push(allImages[i].name);
-    data.push(Math.floor(100 *allImages[i].clickCount / allImages[i].appeared));
+  for(var i = 0; i < allMarketSurveyItems.length; i++){
+    labels.push(allMarketSurveyItems[i].name);
+    data.push(Math.floor(100 *allMarketSurveyItems[i].clickCount / allMarketSurveyItems[i].appeared));
   }
 
 
@@ -164,7 +161,6 @@ function buildMyChart(){
 
 
 
-
 //Instantiating the page
 
 new imageInfo('bag' , 'images/bag.jpg', 'starwars bag');
@@ -190,124 +186,7 @@ new imageInfo('wine-glass', 'images/wine-glass.jpg', 'wine-glass');
 
 
 productList.addEventListener('click', handleClickOnImage);
-
 pickThreeImagesAndIncrementAppeared();
 
 
 
-//==============================================================//
-
-//==============================================================//
-
-
-
-
-
-
-
-
-
-
-
-//==============================================================//
-//       vvvvvvvvvvvvvvvvv
-//==============================================================//
- 
- 
-// function handleClickOnLeftImage(){
-//   //adding clicks
-  
-//   likeCounter++;
-//   var rightImageIndex = Math.floor(Math.random() * allImages.length);
-//   var leftImageIndex = Math.floor(Math.random() * allImages.length);
-//   var centerImageIndex = Math.floor(Math.random() * allImages.length);
-  
-// leftImage.src = allImages[leftImageIndex].filePath;
-// rightImage.src = allImages[rightImageIndex].filePath;
-// centerImage.src = allImages[centerImageIndex].filePath;
-
-
-//   // stop after 25 clicks//
-//   if(likeCounter > 24){
-//     leftImage.removeEventListener('click', handleClickOnLeftImage);
-//     rightImage.removeEventListener('click', handleClickOnRightImage);
-//     centerImage.removeEventListener('click', handleClickOnCenterImage);
-    
-
-//   }
-
-// }
-
-// function handleClickOnRightImage(){
-  
-//   likeCounter++;
-//   var rightImageIndex = Math.floor(Math.random() * allImages.length);
-//   var leftImageIndex = Math.floor(Math.random() * allImages.length);
-//   var centerImageIndex = Math.floor(Math.random() * allImages.length);
-
-//   leftImage.src = allImages[leftImageIndex].filePath;
-// rightImage.src = allImages[rightImageIndex].filePath;
-// centerImage.src = allImages[centerImageIndex].filePath;
-// if(likeCounter > 10){
-//   leftImage.removeEventListener('click', handleClickOnLeftImage);
-//   rightImage.removeEventListener('click', handleClickOnRightImage);
-//   centerImage.removeEventListener('click', handleClickOnCenterImage);
-  
-//   console.log('clicked 25 times');
-// }
-
-// }
-
-// function handleClickOnCenterImage(){
-  
-//   likeCounter++;
-//   var rightImageIndex = Math.floor(Math.random() * allImages.length);
-//   var leftImageIndex = Math.floor(Math.random() * allImages.length);
-//   var centerImageIndex = Math.floor(Math.random() * allImages.length);
-
-// leftImage.src = allImages[leftImageIndex].filePath;
-// rightImage.src = allImages[rightImageIndex].filePath;
-// centerImage.src = allImages[centerImageIndex].filePath;
-// if(likeCounter > 10){
-  
-//   leftImage.removeEventListener('click', handleClickOnLeftImage);
-//   rightImage.removeEventListener('click', handleClickOnRightImage);
-//   centerImage.removeEventListener('click', handleClickOnCenterImage);
-  
-//   console.log('clicked 25 times');
-// }
-// }
-
-
-//==============================================================//
-//         ^^^^^^^^^^
-//==============================================================//
- // new images coming into play//
-// new imageInfo('bag' , 'images/bag.jpg', 'starwars bag');
-// new imageInfo('banana' , 'images/banana.jpg', 'banana');
-// new imageInfo('bathroom' , 'images/bathroom.jpg', 'bathroom');
-// new imageInfo('boots', 'images/boots.jpg', 'Boots');
-// new imageInfo('breakfast', 'images/breakfast.jpg', 'Breakfast maker');
-// new imageInfo('bubblegum', 'images/bubblegum.jpg', 'Meatball bubblegum');
-// new imageInfo('chair', 'images/chair.jpg', 'Chair');
-// new imageInfo('cthulhu', 'images/cthulhu.jpg', 'thulhu');
-// new imageInfo('dog-duck', 'images/dog-duck.jpg', 'Dog Duck');
-// new imageInfo('dragon', 'images/dragon.jpg', 'Dragon');
-// new imageInfo('pen', 'images/pen.jpg', 'Pen');
-// new imageInfo('pet-sweep', 'images/pet-sweep.jpg', 'pet-sweep');
-// new imageInfo('scissors', 'images/scissors.jpg', 'scissors');
-// new imageInfo('shark', 'images/shark.jpg', 'shark');
-// new imageInfo('sweep', 'images/sweep.png', 'sweep');
-// new imageInfo('tauntaun', 'images/tauntaun.jpg', 'tauntaun');
-// new imageInfo('unicorn', 'images/unicorn.jpg', 'unicorn');
-// new imageInfo('usb', 'images/usb.gif', 'usb');
-// new imageInfo('water-can', 'images/water-can.jpg', 'water-can');
-// new imageInfo('wine-glass', 'images/wine-glass.jpg', 'wine-glass');
-
-//==============making it clickable======================//
-
-// leftImage.addEventListener('click', handleClickOnLeftImage);
-
-// rightImage.addEventListener('click', handleClickOnRightImage);
-
-// centerImage.addEventListener('click', handleClickOnCenterImage);
